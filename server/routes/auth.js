07cb2e26912aaa336e3505bee925c0c2074ecc19
Payal Jat <payaljaat55@gmail.com>
2026-06-10 13:11:18 +0530

    feat: update logic in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -55,8 +55,9 @@ router.get("/github/callback", async (req, res) => {
     );
     res.cookie('token', token, {
     httpOnly: true,
-    maxAge: 7 * 24 * 60 * 60 * 1000  
-
+    secure: true,        
+    sameSite: 'none',   
+    maxAge: 7 * 24 * 60 * 60 * 1000
 });
 res.redirect('https://dev-metrics-delta.vercel.app/dashboard');
 })
